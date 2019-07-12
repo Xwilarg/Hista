2821a62349b7132cda1ebc4d6b5629cdef5439e0
Xwilarg <xwilarg@yahoo.fr>
2019-07-12 23:20:57 +0200

    Add empty Discord bot

---
+++ Hista/CommunicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace Hista
+{
+    public class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "Hista", Program.P.client.CurrentUser));
+        }
+    }
+}
